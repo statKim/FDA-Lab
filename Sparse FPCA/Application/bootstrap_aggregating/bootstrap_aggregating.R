868bbddaac1317601a23885f104374c156b1eb76
statKim <hyunsung1021@gmail.com>
2020-01-01 16:50:28 +0900

    [Modify] Bagging for logistic, SVM

--- Sparse FPCA/Application/bootstrap_aggregating/bootstrap_aggregating.R
+++ Sparse FPCA/Application/bootstrap_aggregating/bootstrap_aggregating.R
@@ -3,85 +3,134 @@ setwd("C:\\Users\\user\\Desktop\\KHS\\FDA-Lab\\Sparse FPCA\\Application")
 library(tidyverse)
 library(doParallel)   # parallel computing
 library(fdapace)
-library(rlist)
-library(MASS)
+library(e1071)
 
 # load simulated data
 load("simulated_data.RData")
 
 # parallel computing setting
-ncores <- detectCores() - 3
+ncores <- detectCores() - 2
 registerDoParallel(ncores)
 
-packages <- c("fdapace","MASS")   # foreach에서 사용할 package 정의
+packages <- c("fdapace","e1071")   # foreach에서 사용할 package 정의
 
-
-set.seed(100)
 s <- 10   # sparsity
-# generate data
-i <- 1
-y <- factor(c(rep(0, 100), rep(1, 100)), level=c(0, 1))
-data <- cbind(y, X.curves[[i]])
-ind <- sample(1:nrow(data), 100)   # train set index
-X.train <- as.matrix(data[ind, -1])
-X.test <- as.matrix(data[-ind, -1])
-y.train <- y[ind]
-y.test <- y[-ind]
 
-# sparsify train, test set
-train.data <- Sparsify(X.train, time, sparsity = s)
-test.data <- Sparsify(X.test, time, sparsity = s)
-
-## Bootstrap aggregating
-B <- 10
-N <- nrow(train)
-y.pred <- foreach(b=1:B, .combine="cbind", .packages=packages) %dopar% {
-  boot.ind <- sample(1:N, N, replace=T)
-  
-  fit <- FPCA(train.data$Ly[boot.ind], 
-              train.data$Lt[boot.ind], 
-              optns=list(dataType="Sparse",
-                         methodXi="CE",
-                         methodSelectK="BIC",
-                         verbose=T))
-  K <- fit$selectK
-  fpc.score <- fit$xiEst
-  
-  train <- data.frame(y=y.train, 
-                      x=fpc.score)
-  colnames(train) <- c("y", paste("FPC", 1:K, sep=""))
-  
-  logit <- glm(y~., train, family=binomial)
-  
-  # test data
-  fpc.score.test <- predict(fit, test_data$Ly, test_data$Lt, K=K, xiMethod="CE")
-  test <- data.frame(y=y.test, 
-                     x=fpc.score.test)
-  colnames(test) <- c("y", paste("FPC", 1:K, sep=""))
-  pred <- predict(logit, test, type="response")
-  pred <- ifelse(y.pred > 0.5, 1, 0)
-  
-  return(pred)
-  # if (b==1) {
-  #   y.pred <- pred
-  # } else {
-  #   y.pred <- cbind(y.pred, 
-  #                   pred)
-  # }
-}
-
-# 2 class majority voting functon
-majority_vote <- function(x) {
-  key <- unique(x)
-  if (length(key) > 1) {
-    val <- which.max( table(y.pred[1, ]) ) - 1
-  } else {
-    val <- key
+system.time({   # running time check
+for (s in 2:18) {
+  print(paste("s=", s, sep=""))
+  for (i in 1:100) {
+    ## generate data
+    # i <- 1
+    set.seed(100)
+    y <- factor(c(rep(0, 100), rep(1, 100)), level=c(0,1))
+    ind <- sample(1:length(y), 100)   # train set index
+    X.train <- as.matrix( X.curves[[i]][ind, ] )
+    X.test <- as.matrix( X.curves[[i]][-ind, ] )
+    y.train <- y[ind]
+    y.test <- y[-ind]
+    
+    # sparsify train, test set
+    train.data <- Sparsify(X.train, time, sparsity = s)
+    test.data <- Sparsify(X.test, time, sparsity = s)
+    
+    ## Bootstrap aggregating for classification
+    B <- 100
+    N <- nrow(X.train)
+    y.pred <- foreach(b=1:B, .packages=packages) %dopar% {
+      # fit FPCA for bootstrapped data
+      boot.ind <- sample(1:N, N, replace=T)
+      fpca.fit <- FPCA(train.data$Ly[boot.ind], 
+                       train.data$Lt[boot.ind], 
+                       optns=list(dataType="Sparse",
+                                  methodXi="CE",
+                                  # methodSelectK="BIC",
+                                  FVEthreshold=0.99,
+                                  verbose=T))
+      k <- fpca.fit$selectK   # optimal number of PCs when using BIC
+      fpc.score <- fpca.fit$xiEst
+      
+      # train FPC scores
+      train.fpc <- data.frame(y=y.train, 
+                              x=fpc.score)
+      colnames(train.fpc) <- c("y", paste("FPC", 1:k, sep=""))
+      
+      # fit the classification model using bootstrapped FPC scores
+      fit.logit <- glm(y~., train.fpc, family=binomial)
+      fit.svm.linear <- tune(svm, y ~ ., data=train.fpc, kernel="linear",
+                             ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))$best.model
+      fit.svm.radial <- tune(svm, y ~ ., data=train.fpc, kernel="radial",
+                             ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))$best.model
+      fit.svm.sigmoid <- tune(svm, y ~ ., data=train.fpc, kernel="sigmoid",
+                              ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))$best.model
+      fit.svm.poly <- tune(svm, y ~ ., data=train.fpc, kernel="poly",
+                           ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))$best.model
+      
+      # test FPC scores
+      fpc.score.test <- predict(fpca.fit, test.data$Ly, test.data$Lt, K=k, xiMethod="CE")
+      test.fpc <- data.frame(y=y.test, 
+                             x=fpc.score.test)
+      colnames(test.fpc) <- c("y", paste("FPC", 1:k, sep=""))
+      
+      # predict
+      pred.logit <- predict(fit.logit, test.fpc, type="response")
+      pred.logit <- factor(ifelse(pred.logit > 0.5, 1, 0), levels=c(0, 1))
+      pred.svm.linear <- predict(fit.svm.linear, test.fpc)
+      pred.svm.radial <- predict(fit.svm.radial, test.fpc)
+      pred.svm.sigmoid <- predict(fit.svm.sigmoid, test.fpc)
+      pred.svm.poly <- predict(fit.svm.poly, test.fpc)
+      
+      return( list(logit=pred.logit,
+                   svm.linear=pred.svm.linear,
+                   svm.radial=pred.svm.radial,
+                   svm.sigmoid=pred.svm.sigmoid,
+                   svm.poly=pred.svm.poly) )
+    }
+    
+    
+    # majority voting functon
+    majority_vote <- function(x) {
+      key <- sort(unique(x))
+      val <- key[which.max( table(x) )]
+      return(val)
+    }
+    
+    # save the accuracy
+    cname <- c("logit","svm.linear","svm.radial","svm.sigmoid","svm.poly")
+    acc <- c()
+    for (j in 1:5) {
+      pred <- apply(sapply(y.pred, function(x){ cbind( x[[j]] ) }), 
+                    1, 
+                    majority_vote)
+      pred <- factor(pred-1, levels=c(0,1))
+      
+      acc[j] <- mean(pred == y.test)
+    }
+    
+    if (i == 1) {
+      acc.result <- acc
+    } else {
+      acc.result <- rbind(acc.result, acc)
+    }
   }
   
-  return(val)
+  acc.result <- colMeans(acc.result)
+    
+  if (s == 2) {
+    result <- data.frame(matrix(acc.result, nrow=1))
+    colnames(result) <- cname
+  } else {
+    result <- rbind(result, acc.result)
+  }
+  
+  if (s == 18) { rownames(result) <- paste("s=", 2:18, sep="") }
 }
+})   # running time check
 
-pred <- apply(y.pred, 1, majority_vote)
 
-table(pred, y.test)
+table(Pred=pred, 
+      True=y.test)
+mean(pred == y.test)
+
+
+
