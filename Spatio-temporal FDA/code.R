b342243ccacb73bc98218f91da2e92bfe6f51595
statKim <hyunsung1021@gmail.com>
2020-05-23 16:31:54 +0900

    [Modify] image plot 추가

--- Spatio-temporal FDA/code.R
+++ Spatio-temporal FDA/code.R
@@ -1,5 +1,5 @@
 
-load("C:/Users/hyunsungKim/Dropbox/lab/KHS/hourly_data.RData")
+load("C:/Users/user/Google 드라이브/Lab/KHS/spatio-temporal/hourly_data.RData")
 ls()
 dim(full_data)
 head(full_data)
@@ -9,22 +9,25 @@ library(ftsa)
 library(MFPCA)
 
 
-################ ################ ################ ################ ################ 
-################  generate data   : X ( time=24*60 days (2014/01/01~2014/3/1), grid=50 in Korea )####################### 
-################ ################ ################ ################ ################ 
+########################################
+### generate data
+### - 24*60 days (2014/01/01~2014/3/1) 
+### - grid=50 in Korea
+########################################
 grid_pt <- unique(full_data$grid)[1:50]   # grid 변수의 unique한 값 50개를 뽑음
 X <- vector()
-for (i in 1:length(grid_pt)){   # grid별로 각ㄱ 24*60개의 시계열 데이터를 뽑아서 rbind
-	X <- rbind(X, full_data[which(full_data$grid==grid_pt[i]), ][1:(24*60),] )
+for (i in 1:length(grid_pt)){   # grid별로 각각 24*60개의 시계열 데이터를 뽑아서 rbind
+	X <- rbind(X, 
+	           full_data[which(full_data$grid==grid_pt[i]), ][1:(24*60), ])
 }
 colnames(X)[7] <- 'Rdate'
 X$time_in_day <- rep(c(1:24), 60)   # 시간 변수 추가(60일간의 시간별 데이터)
 head(X)
 
+# Interpolate that PM10 is NA
 na_index <- which(is.na(X$PM10)==T)   # PM10이 NA인 값의 index 저장
-
 for (k in 1:length(na_index)){   # NA값들을 인접한 10개 데이터의 평균으로 보간
-	X$PM10[na_index[k] ] <- mean( X$PM10[ c( (na_index[k]-5):(na_index[k]+5) ) ], na.rm=T)   ## interpolate NA terms
+	X$PM10[na_index[k]] <- mean(X$PM10[(na_index[k]-5):(na_index[k]+5)], na.rm=T)   # interpolate NA terms
 }
 
 
@@ -35,96 +38,112 @@ k <- 1
 grid1 <- X[which(X[,1] == grid_pt[k]), ]
 
 i <- 1
-tt <- c( (1 + 24*(i-1)):(24*i) ) 
-plot(grid1$Rdate[tt], grid1$PM10[tt], type='l', main=paste('grid ID =', grid_pt[k]), xlab='Time of day', ylab="PM10", ylim=range(grid1$PM10, na.rm=T) )
+tt <- (1 + 24*(i-1)):(24*i)
+plot(grid1$Rdate[tt], grid1$PM10[tt], type='l', main=paste('grid ID =', grid_pt[k]), 
+     xlab='Time of day', ylab="PM10", ylim=range(grid1$PM10, na.rm=T))
 for (i in 2:60){
-  tt <- c( (1 + 24*(i-1)):(24*i) ) 
+  tt <- (1 + 24*(i-1)):(24*i) 
 	lines(grid1$Rdate[1:24], grid1$PM10[tt], col=i)
 }
 
 
 ## Smoothing the data 
-smoothed_X <- matrix(nrow=length(unique(X$grid)), ncol=60*24)  ## grid(50) * time(24*60)
+smoothed_X <- matrix(nrow=length(unique(X$grid)), ncol=60*24)  # grid(50) * time(24*60)
 dim(smoothed_X)
 
 for (k in 1:length(unique(X$grid))){
 	grid1 <- X[which(X[,1] == grid_pt[k]), ]
 	for (i in 1:60){
-  	tt <- c( (1 + 24*(i-1)):(24*i) ) 
+  	tt <- (1 + 24*(i-1)):(24*i)
   	b <- gam(PM10[tt]~s(time_in_day[tt], bs='cc', k=24), data=grid1, method="REML")
   	smoothed_X[k, tt] <- b$fitted.values
 	}
 }
 
+
+
+library(fields)
+image.plot(1:24, 1:60, matrix(smoothed_X[2, ], 24, 60),
+           xlab="Time of day", ylab="Days")
+image.plot(1:24, 1:60, image.smooth(matrix(smoothed_X[2, ], 24, 60))$z,
+           xlab="Time of day", ylab="Days")
+
+image.smooth(matrix(smoothed_X[2, ], 24, 60),
+             nrow=48, ncol=120)
+
 k <- 1
 i <- 1
-tt <- c( (1+24*(i-1) ):(24*i) ) 
-plot(grid1$Rdate[tt], smoothed_X[k,tt], type='l', main=paste('grid ID =', grid_pt[k]), xlab='Time of day', ylab="PM10", ylim=range(smoothed_X[k,], na.rm=T))
+tt <- (1+24*(i-1) ):(24*i)
+plot(grid1$Rdate[tt], smoothed_X[k,tt], type='l', 
+     main=paste('grid ID =', grid_pt[k]), xlab='Time of day', ylab="PM10", ylim=range(smoothed_X[k, ], na.rm=T))
 for (i in 2:60){
-	tt <- c( (1+24*(i-1) ):(24*i) ) 
-	lines(grid1$Rdate[1:24], smoothed_X[k,tt], col=i)
+	tt <- (1+24*(i-1) ):(24*i)
+	lines(grid1$Rdate[1:24], smoothed_X[k, tt], col=i)
 }
 
 
-#####  FPCA  (Need to apply into two type of grid (ex. grassland vs forest))
+### FPCA  (Need to apply into two type of grid (ex. grassland vs forest))
 grass_data <- matrix(apply(smoothed_X[1:25,], 2, mean), 24, 60)
 forest_data <- matrix(apply(smoothed_X[26:50,], 2, mean), 24, 60)
+colnames(grass_data) <- 1:60
+colnames(forest_data) <- 1:60
 
-
-fts_object_grass <- fts(x=c(1:24), grass_data)   # fts(): functional 객체 생성
-fit_grass <- ftsm(fts_object_grass, order=3, method='classical')   # ftsm(): functional time series model - fpca
+fts_object_grass <- fts(x=c(1:24), grass_data)   # convert to fts object(to use ftsm function)
+fit_grass <- ftsm(fts_object_grass, order=3, method='classical')   # fit fpca with 3 FPCs
 
 fts_object_forest <- fts(x=c(1:24), forest_data)  
 fit_forest <- ftsm(fts_object_forest, order=3, method='classical')
 
+# plot of mean and FPC functions
 par(mfrow=c(1,4))
-plot(fit_grass$basis[,1], type='l', main='mean function', xlab='Time of day', ylab='', ylim=range(fit_grass$basis[,1], fit_forest $basis[,1]))
-lines(fit_forest$basis[,1], col=2)
-# legend('topright', c('grass', 'forest'), lty=1, col=c(1,2))
+plot(fit_grass$basis[,1], type='l', main='mean function', col="red",
+     xlab='Time of day', ylab='', ylim=range(fit_grass$basis[,1], fit_forest $basis[,1]))
+lines(fit_forest$basis[,1], col="blue")
+legend('topright', c('grass', 'forest'), lty=1, col=c("red","blue"))
 for (k in 2:4){
-	plot(fit_grass$basis[,k], type='l', main=paste(k-1,'-th PC (phi)', sep=""), xlab='Time of day' , ylab='', ylim=range(fit_grass$basis[,k], fit_forest$basis[,k]))
-	lines(fit_forest$basis[,k], col=2)
-	# legend('topright', c('grass', 'forest'), lty=1, col=c(1,2))
+	plot(fit_grass$basis[,k], type='l', main=paste(k-1,'-th PC function', sep=""), col="red",
+	     xlab='Time of day' , ylab='', ylim=range(fit_grass$basis[,k], fit_forest$basis[,k]))
+	lines(fit_forest$basis[,k], col="blue")
 }
 
 
-#####  FPCA  (for each grid))
-z_g <- list()
-z_f <- list()
-for (q in 1:3) {
-  z_g[[q]] <- matrix(nrow=60 , ncol=50/2)
-  z_f[[q]] <- matrix(nrow=60 , ncol=50/2)
-  for (k in 1:25) {
-    grass_data <- matrix(smoothed_X[k,], nrow=24, ncol=60)   # matrix( apply(smoothed_X[1:25,],2,mean) , 24,60)
-    forest_data <- matrix(smoothed_X[k+25,], nrow=24, ncol=60)   # matrix(apply(smoothed_X[26:50,],2,mean), 24,60)
-    
-    fts_object_grass <- fts(x=1:24, grass_data)   # fts(): functional 객체 생성
-    fit_grass <- ftsm(fts_object_grass, order=3, method='classical')   # ftsm(): functional time series model - fpca
-    
-    fts_object_forest <- fts(x=1:24, forest_data)  
-    fit_forest <- ftsm(fts_object_forest, order=3, method='classical')
-    
-    z_g[[q]][,k] <- as.vector(fit_grass$coeff[,q+1])
-    z_f[[q]][,k] <- as.vector(fit_forest$coeff[,q+1])
-  }
-}
+# #####  FPCA  (for each grid))
+# z_g <- list()
+# z_f <- list()
+# for (q in 1:3) {
+#   z_g[[q]] <- matrix(nrow=60 , ncol=50/2)
+#   z_f[[q]] <- matrix(nrow=60 , ncol=50/2)
+#   for (k in 1:25) {
+#     grass_data <- matrix(smoothed_X[k,], nrow=24, ncol=60)   # matrix( apply(smoothed_X[1:25,],2,mean) , 24,60)
+#     forest_data <- matrix(smoothed_X[k+25,], nrow=24, ncol=60)   # matrix(apply(smoothed_X[26:50,],2,mean), 24,60)
+# 
+#     fts_object_grass <- fts(x=1:24, grass_data)   # fts(): functional 객체 생성
+#     fit_grass <- ftsm(fts_object_grass, order=3, method='classical')   # ftsm(): functional time series model - fpca
+# 
+#     fts_object_forest <- fts(x=1:24, forest_data)
+#     fit_forest <- ftsm(fts_object_forest, order=3, method='classical')
+# 
+#     z_g[[q]][,k] <- as.vector(fit_grass$coeff[,q+1])
+#     z_f[[q]][,k] <- as.vector(fit_forest$coeff[,q+1])
+#   }
+# }
+# 
+# # 25th, 50th의 PC function
+# par(mfrow=c(1,4))
+# plot(fit_grass$basis[,1], type='l', main='mean function', xlab='Time of day', ylab='', col="red",
+#      ylim=range(fit_grass$basis[,1], fit_forest$basis[,1]))
+# lines(fit_forest$basis[,1], col="blue")
+# legend('topleft', c('grass', 'forest'), lty=1, col=c("red","blue"))
+# for(k in 2:4){
+#   plot(fit_grass$basis[,k], type='l', main=paste(k-1, '-th PC (phi)', sep=""), xlab='Time of day', col="red",
+#        ylab='', ylim=range(fit_grass$basis[,k], fit_forest$basis[,k]))
+#   lines(fit_forest$basis[,k], col="blue")
+#   # legend('topright', c('grass', 'forest'), lty=1,col=c(1,2))
+# }
 
-# 25th, 50th의 PC score
-par(mfrow=c(1,4))
-plot(fit_grass$basis[,1], type='l', main='mean function', xlab='Time of day', ylab='',
-     ylim=range(fit_grass$basis[,1], fit_forest$basis[,1]))
-lines(fit_forest$basis[,1], col=2)
-# legend('topright', c('grass', 'forest'), lty=1, col=c(1,2))
-for(k in 2:4){
-  plot(fit_grass$basis[,k], type='l', main=paste(k-1, '-th PC (phi)', sep=""), xlab='Time of day',
-       ylab='', ylim=range(fit_grass$basis[,k], fit_forest$basis[,k]))
-  lines(fit_forest$basis[,k], col=2)
-  # legend('topright', c('grass', 'forest'), lty=1,col=c(1,2))
-}
 
 
-
-#### Smoothed ANOVA
+### Smoothed ANOVA (location + days + loc*days) - using gam!!
 
 ## 1. grass
 unq.grid <- unique(X$grid)[1:25]
