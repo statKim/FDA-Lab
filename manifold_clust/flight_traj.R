cf655fe52c2b741499403fc21a8a9e6e53bd1402
statKim <hyunsung1021@gmail.com>
2022-02-21 13:46:14 +0900

    [Add] flight 데이터 전처리

--- manifold_clust/flight_traj.R
+++ manifold_clust/flight_traj.R
@@ -308,9 +308,239 @@ AND NONNULLVALUE(callsign);"
     }
     save(flight, flight_state, file = "RData/flight.RData")
 }
+dim(flight_state)   # 69756853       17
 
 ### Terminate the connection
 osn_disconnect(session)
+
+
+
+#####################################################################
+### Data preprocessing
+#####################################################################
+
+### Check the number of missings
+### Caution!! Because of too large data, it takes a lot of times. (8 mins)
+apply(flight_state, 2, function(col){ sum(is.na(col)) })
+# time        icao24           lat           lon      velocity       heading      vertrate      callsign      onground 
+# 0             0             0             0        720501        720501        720373             0             0 
+# alert           spi        squawk  baroaltitude   geoaltitude lastposupdate   lastcontact          hour 
+# 0             0       4525967       1079393       2664876             0             0             0 
+
+### Transform unix time to date
+### Create minute and day variables
+### 7 mins
+flight_state_to_LA <- flight_state %>% 
+    arrange(time) %>% 
+    select(time, icao24, lat, lon, vertrate, callsign, onground, hour) %>% 
+    mutate(time = unixtime2date(time),
+           hour = unixtime2date(hour),
+           minute = substr(time, 1, 16),
+           day = substr(time, 1, 10)) %>% 
+    group_by(icao24, callsign, minute) %>% 
+    filter(row_number() == n()) %>%    # select last row
+    ungroup()
+dim(flight_state_to_LA)   # 1188867      10
+flight_state_to_LA
+save(flight_to_LA, flight_state_to_LA, file = "RData/flight_reduced.RData")
+
+
+### Test trajectories
+sub3 <- flight_to_LA %>% 
+    # filter(icao24 == flight_id$icao24[1]) %>% 
+    select(icao24, callsign, day, airline) %>% 
+    left_join(flight_state_to_LA, by = c("icao24", "day", "callsign")) %>% 
+    drop_na()   # remove to trajectories of icao24 for such day
+dim(sub3)   # 11610    11
+# apply(sub3, 2, function(col){ sum(is.na(col)) })
+# sub3[which(is.na(sub3$time)), ]
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+map_bg <- ggplot(data = world) +
+    geom_sf() +
+    coord_sf(xlim = range(sub3$lon) + c(-10, 10),
+             ylim = range(sub3$lat) + c(-10, 10), 
+             expand = FALSE)
+map_bg + 
+    geom_path(
+        data = sub3, 
+        aes(
+            x = lon, 
+            y = lat, 
+            group = day,
+            color = airline
+        )
+    ) +
+    theme_bw()
+
+sub3[which(sub3$lat > 50), ]
+sub3 %>% 
+    filter(icao24 == "71be27" & day == "2019-06-23") %>% 
+    as.data.frame()
+
+### Remove un-usual trajectory (it seems to be malfunction of the ADS-B reciever)
+sub3 <- sub3 %>% 
+    filter( !(icao24 == "71be27" & day == "2019-06-23") ) 
+dim(sub3)   # 11536    11
+sub3
+
+
+### RKSI - lat: 37.46, lon: 126.44
+### KLAX - lat: 33.94, lon: -118.41
+sub3 %>% 
+    group_by(icao24, callsign, day) %>% 
+    arrange(time) %>% 
+    filter(row_number() == 1) %>%     # select 1st row
+    ungroup() %>% 
+    select(icao24, callsign, time, lat, lon, vertrate) %>% 
+    mutate(lat = round(lat, 2),
+           lon = round(lon, 2)) %>% 
+    as.data.frame()
+sub3 %>% 
+    group_by(icao24, callsign, day) %>% 
+    arrange(time) %>% 
+    filter(row_number() == n()) %>%     # select last row
+    ungroup() %>% 
+    select(icao24, callsign, time, lat, lon, vertrate) %>% 
+    mutate(lat = round(lat, 2),
+           lon = round(lon, 2)) %>% 
+    as.data.frame()
+
+
+sub3 %>% 
+    filter(round(lat, 1) == 37.5 & round(lon, 1) == 126.4) %>% 
+    select(icao24, callsign, day, time, lat, lon) %>% 
+    group_by(icao24, callsign, day) %>% 
+    filter(row_number() == 1) %>% 
+    as.data.frame()
+
+
+sub3 %>% 
+    filter(round(lat, 1) == 33.9 & round(lon, 1) == -118.4) %>% 
+    select(icao24, callsign, day, time, lat, lon) %>% 
+    group_by(icao24, callsign, day) %>% 
+    filter(row_number() == 1) %>% 
+    as.data.frame()
+# count > 2이상인거??
+
+
+
+### Obtain Take-off and Landing time
+### - Incheon(RKSI) -> lat: 37.46, lon: 126.44
+### - LA(KLAX) -> lat: 33.94, lon: -118.41
+
+# Take-off time for RKSI location
+takeoff <- flight_state_to_LA %>% 
+    filter( (lat > 36.46 & lat < 38.46) & (lon > 125.44 & lon < 127.44) ) %>% 
+    arrange(time) %>%
+    select(time, icao24, lat, lon, vertrate, callsign, onground, hour) %>% 
+    mutate(time = unixtime2date(time),
+           hour = unixtime2date(hour),
+           minute = substr(time, 1, 16),
+           day = substr(time, 1, 10)) %>% 
+    group_by(icao24, callsign, day) %>% 
+    filter(row_number() == n()) %>% 
+    ungroup() %>% 
+    select(time, icao24, callsign, minute, day) %>% 
+    mutate(day = as.Date(day),
+           takeoff = time)
+takeoff
+
+# Landing time for KLAX location
+landing <- flight_state_to_LA %>% 
+    filter( (lat > 32.94 & lat < 34.94) & (lon > -119.41 & lon < -117.41) ) %>% 
+    arrange(time) %>%
+    select(time, icao24, lat, lon, vertrate, callsign, onground, hour) %>% 
+    mutate(time = unixtime2date(time),
+           hour = unixtime2date(hour),
+           minute = substr(time, 1, 16),
+           day = substr(time, 1, 10)) %>% 
+    group_by(icao24, callsign, day) %>% 
+    filter(row_number() == 1) %>% 
+    ungroup() %>% 
+    select(time, icao24, callsign, minute, day) %>% 
+    mutate(day = as.Date(day),
+           landing = time)
+landing
+
+
+# Each flight flies on once per 1 day
+takeoff %>% 
+    group_by(icao24, callsign, day) %>% 
+    summarise(n = n()) %>% 
+    filter(n > 1)
+landing %>% 
+    group_by(icao24, callsign, day) %>% 
+    summarise(n = n()) %>% 
+    filter(n > 1)
+
+# Take-off before 15:00
+takeoff_landing_1 <- takeoff %>% 
+    left_join(landing, by = c("icao24", "callsign", "day")) %>% 
+    drop_na() %>% 
+    select(icao24, callsign, takeoff, landing) %>% 
+    mutate(duration = landing - takeoff) %>% 
+    filter(duration > 0)   # remove negative flying time
+takeoff_landing_1
+takeoff_landing_1$duration
+
+# Take-off after 15:00 (Day is changed!!)
+takeoff_landing_2 <- takeoff %>% 
+    mutate(day = as.Date(day) + 1) %>% 
+    left_join(landing, by = c("icao24", "callsign", "day")) %>% 
+    drop_na() %>% 
+    select(icao24, callsign, takeoff, landing) %>% 
+    mutate(duration = landing - takeoff) %>% 
+    filter(duration < 15)   # remove too large flying time
+takeoff_landing_2
+takeoff_landing_2$duration
+
+# Combine
+takeoff_landing <- rbind(takeoff_landing_1,
+                         takeoff_landing_2) %>% 
+    arrange(takeoff) %>% 
+    mutate(flight_id = row_number(),
+           airline = substr(callsign, 1, 3))
+takeoff_landing   # 590  5
+takeoff_landing$icao24 %>% 
+    unique()
+table(takeoff_landing$airline)
+# AAR GTI KAL 
+# 231  53 306 
+# Asiana Airlines, Atlas Air, Korean Air
+
+
+### Get data for each takeoff, landing
+flight_df <- takeoff_landing %>% 
+    left_join(flight_state_to_LA, 
+              by = c("icao24","callsign")) %>% 
+    filter(time >= takeoff & time <= landing) %>% 
+    select(flight_id, time, icao24, airline, callsign, lat, lon, takeoff, landing, duration)
+flight_df   # 43326  10
+
+
+### Draw trajectories again
+world <- ne_countries(scale = "medium", returnclass = "sf")
+map_bg <- ggplot(data = world) +
+    geom_sf() +
+    coord_sf(xlim = range(flight_df$lon) + c(-10, 10),
+             ylim = range(flight_df$lat) + c(-5, 5), 
+             expand = FALSE)
+map_bg + 
+    geom_path(
+        data = flight_df, 
+        aes(
+            x = lon, 
+            y = lat, 
+            group = flight_id,
+            color = airline
+        ),
+        size = 0.3
+    ) +
+    theme_bw()
+
+
+
 
 
 
