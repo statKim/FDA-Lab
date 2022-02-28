d08a644746b66b9c7d6a1a3c333cde104e903672
statKim <hyunsung1021@gmail.com>
2022-02-28 11:11:24 +0900

    [Update] peca function to obtain k PECs

--- functional_quantile/peca.R
+++ functional_quantile/peca.R
@@ -1,137 +1,159 @@
-############################################
+############################################################
 ### Principal expectile component analysis
 ### Tran et al. (2019)
 ### - PrincipalExpectile algorithm
-############################################
-
-### Example
-# devtools::install_github("obleeker/quant.pca")
-library(quant.pca)
-
-# generate data
-set.seed(100)
-n = 100
-X = data.frame(cbind(rnorm(n),rnorm(n),rnorm(n)))
-
-# running PQC on the 0.9-quantile 
-pec.fit <- pqcomp(data = Y, 
-                  projDim = 2,
-                  tau = 0.9, 
-                  lambda = 0.1, 
-                  muEst = T)
-matplot(pec.fit$loadings, type = "l")
-
-
-peca.obj <- peca(Y, tau = 0.9)
-
-plot(pec.fit$loadings[, 1], type = "l")
-lines(peca.obj$pec, col = 2)
-
-
-set.seed(100)
-# n <- 100
-# p <- 51
-# Y <- mvtnorm::rmvnorm(n = n, mean = rep(0, p))
-x <- robfpca::sim_delaigle(n = 100,
-                           type = "dense",
-                           out.prop = 0,
-                           dist = "normal")
-Y <- robfpca::list2matrix(x)   # transform list to matrix
-matplot(t(Y), type = "l")
-
-peca.obj <- peca(Y, tau = 0.7)
-
-matplot(t(Y), type = "l", col = "gray")
-lines(peca.obj$pec, type = "l", lwd = 2)
-
+############################################################
 
 ### Principal expectile component analysis using PrincipalExpectile algorithm
-### 확인이 필요
-peca <- function(Y, tau = 0.5, max.iter = 30) {
-  n <- nrow(Y)
-  p <- ncol(Y)
+### - Y : nxp data matrix (dense functional data)
+### - tau : tau percent quantile between 0 and 1
+### - k : the number of PECs
+### - max.iter : the number of maximum iterations for each PEC
+peca <- function(Y, tau = 0.5, k = 2, max.iter = 30) {
+  n <- nrow(Y)   # number of curves
+  p <- ncol(Y)   # number of timepoints
   
-  # tau <- 0.7   # tau percent quantile
-  # max.iter <- 30   # the number of maximum iterations
-  
-  iter <- 0   # initial iteration
-  # w <- matrix(ifelse(runif(n*p) > tau, tau, 1-tau), ncol = p)   # initial weights (다시 체크하기!!)
-  w <- ifelse(runif(n) > tau, tau, 1-tau)
-  w_update <- rep(0, n)
-  
-  # Iteration
-  while (sum(w - w_update) != 0) {
-    tau_pos <- which(w == tau)
-    tau_neg <- which(w != tau)
-    
-    # Compute e_tau
-    num <- tau * colSums(Y[tau_pos, ]) + (1-tau) * colSums(Y[tau_neg, ])
-    den <- tau * length(tau_pos) + (1-tau) * length(tau_neg)
-    e_tau <- num / den
-    
-    # Compute C_tau
-    Y_center <- Y - matrix(rep(e_tau, n), ncol = p, byrow = T)
-    C_tau <- (tau/n) * (t(Y_center[tau_pos, ]) %*% Y_center[tau_pos, ] ) + ((1-tau)/n) * (t(Y_center[tau_neg, ]) %*% Y_center[tau_neg, ] )
-    
-    # Set v into largest eivenvector of C_tau %*% C_tau^T
-    eig <- eigen(C_tau %*% t(C_tau), symmetric = T)
-    v <- matrix(eig$vectors[, 1], ncol = 1)
-    
-    # Set mu_tau into tau-expectile of v^T %*% Y_i
-    # t(v) %*% Y[1, ]
-    v_Y <- as.numeric(Y %*% v)
-    mu_tau <- expectile(v_Y, probs = tau)
-    
-    # Update w_i
-    w_update <- ifelse(as.numeric(Y %*% v) > mu_tau, tau, 1-tau)
-    
-    iter <- iter + 1
-    
-    if (iter > max.iter) {
-      break
+  ### Each PEC is obtained by sequentially
+  pec <- matrix(0, nrow = p, ncol = k)   # principal expectile component
+  iter.pec <- rep(0, k)   # number of iteration for each component
+  resid <- Y
+  for (i in 1:k) {
+    # Obtain residuals
+    if (i > 1) {
+      resid <- resid - (resid %*% v + mu_tau) %*% t(v)
     }
+    
+    # Initialize weights
+    w <- ifelse(runif(n) > 0.5, tau, 1-tau)
+    w_before <- rep(0, n)
+    
+    ### Do PrincipalExpectile algorithm (See, Algorithm 1 in Tran et al.(2019))
+    iter <- 0   # initial iteration
+    while (sum(w - w_before) != 0) {
+      # Obtain index set
+      tau_pos <- which(w == tau)
+      tau_neg <- which(w != tau)
+      
+      # Compute e_tau
+      num <- tau * colSums(resid[tau_pos, ]) + (1-tau) * colSums(resid[tau_neg, ])
+      den <- tau * length(tau_pos) + (1-tau) * length(tau_neg)
+      e_tau <- num / den
+      
+      # Compute C_tau
+      Y_center <- resid - matrix(rep(e_tau, n), ncol = p, byrow = T)
+      C_tau <- (tau/n) * (t(Y_center[tau_pos, ]) %*% Y_center[tau_pos, ] ) + ((1-tau)/n) * (t(Y_center[tau_neg, ]) %*% Y_center[tau_neg, ] )
+      
+      # Set v into largest eivenvector of C_tau %*% C_tau^T
+      eig <- eigen(C_tau %*% t(C_tau), symmetric = T)
+      v <- matrix(eig$vectors[, 1], ncol = 1)
+      
+      # Set mu_tau into tau-expectile of v^T %*% Y_i
+      # t(v) %*% Y[1, ]
+      v_Y <- as.numeric(resid %*% v)
+      mu_tau <- expectile(v_Y, probs = tau)
+      
+      # Update w_i
+      w_before <- w
+      w <- ifelse(v_Y > mu_tau, tau, 1-tau)
+      
+      # Update iteration number
+      iter <- iter + 1
+      
+      # Too many iterations, breaks
+      if (iter > max.iter) {
+        break
+      }
+    }
+    
+    if (i == 1) {
+      tau_expect <- e_tau   # tau-expectile
+    }
+    pec[, i] <- v
+    iter.pec[i] <- iter
   }
   
+  # Output
   obj <- list(
-    pec = v,   # 1st principal expectile component of Y
-    num.iter = iter
+    center = tau_expect,  # tau-expectile (corresponding to mean function)
+    pec = pec,            # first k PECs
+    num.iter = iter.pec   # the iteration number of the algorithm
   )
   
   return(obj)
 }
 
 
-
-### From "expectreg" 수정해야됨!!
-expectile <- function(x, probs = 0.5, dec = 4) {
-  if (!is.vector(x)) 
-    stop("observations are needed in vector form.")
-  if (min(probs) < 0 || max(probs) > 1) 
-    stop("only asymmetries between 0 and 1 allowed.")
-  e = mean(x)
-  ee = 0 * probs
-  g = max(abs(x)) * 1e-06
-  for (k in 1:length(probs)) {
-    p = probs[k]
-    if (p == 0) 
-      ee[k] = min(x, na.rm = TRUE)
-    else if (p == 1) 
-      ee[k] = max(x, na.rm = TRUE)
-    else {
-      for (it in 1:20) {
-        w = ifelse(x < e, 1 - p, p)
-        enew = sum(w * x)/sum(w)
-        de = max(abs(enew - e))
-        e = enew
-        if (de < g) 
+### Get expectile
+### - x : vector
+### - probs : quantile between 0 and 1
+### - max.iter : the maximum iteration numbers
+expectile <- function(x, probs = 0.5, max.iter = 30) {
+  m <- mean(x)
+  expect <- 0 * probs
+  tol <- max(abs(x)) * 1e-6
+  
+  for (i in 1:length(probs)) {
+    p <- probs[i]
+    
+    if (p == 0) {
+      expect[i] <- min(x, na.rm = TRUE)
+    } else if (p == 1) {
+      expect[i] <- max(x, na.rm = TRUE)
+    } else {
+      for (iter in 1:max.iter) {
+        w <- ifelse(x < m, 1 - p, p)
+        m_new <- sum(w * x) / sum(w)
+        m_diff <- max(abs(m_new - m))
+        m <- m_new
+        if (m_diff < tol) {
           break
+        }
       }
-      ee[k] = e
+      expect[i] <- m
     }
   }
-  names(ee) = probs
-  ee = round(ee, dec)
-  return(ee)
+  
+  names(expect) <- probs
+  
+  return(expect)
 }
-  
-  
+
+
+
+
+############################################
+### Example
+############################################
+
+### Generate example data
+set.seed(100)
+x <- robfpca::sim_delaigle(n = 100,
+                           type = "dense",
+                           out.prop = 0,
+                           dist = "tdist")
+Y <- robfpca::list2matrix(x)   # transform list to matrix
+matplot(t(Y), type = "l")
+
+### Check the method is same with conventional PCA
+peca.obj <- peca(Y, tau = 0.5, k = 3)    # PEC
+pca.obj <- eigen(cov(Y))$vectors[, 1:3]  # conventional PCA 
+
+matplot(peca.obj$pec, type = "l", col = 1)
+matlines(pca.obj, col = 2)
+
+
+### 80% expectile components
+peca.obj2 <- peca(Y, tau = 0.9, k = 3)
+
+matplot(peca.obj$pec, type = "l", col = 1)   # PCA
+matlines(peca.obj2$pec, col = 2)   # 80% PEC
+
+
+### Mean and expectile function
+plot(colMeans(Y), type = "l", ylim = c(-1, 1))   # meanfunction
+lines(peca.obj$center, col = 2)   # 50% expectile function (same with mean)
+lines(peca.obj2$center, col = 3)  # 80% expectile function
+
+
+
+
