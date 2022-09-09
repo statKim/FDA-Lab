129db2bde53c4e22e3c21e525effb643ca7f1e80
statKim <hyunsung1021@gmail.com>
2022-09-09 16:27:28 +0900

    [Add] S3 simulation

--- manifold_clust/figure.R
+++ manifold_clust/figure.R
@@ -33,7 +33,7 @@ if (num.pc.method == "FVE") {
 }
 
 ### Simulation for 3 types of data
-r3dDefaults$windowRect <- c(0, 0, 800, 400) 
+r3dDefaults$windowRect <- c(0, 0, 800, 350) 
 open3d()
 mfrow3d(1, 3)   # par(mfrow = c(2, 1))
 for (sim.type in 1:3) {
@@ -121,18 +121,131 @@ for (sim.type in 1:3) {
     #     x1 <- t(Ly[[i]])
     #     plot3d(x1, type = "l", col = cluster[i], lwd = 1, add = T)
     # }
-    rgl.spheres(0, 0, 0, radius = 1, col = "gray", alpha = 0.6, back = "lines")
+    rgl.spheres(0, 0, 0, radius = 0.99, col = "gray", alpha = 0.3, back = "lines",
+                fastTransparency = F, add = T)
+    # rgl.spheres(0, 0, 0, radius = 1, col = "gray", alpha = 0.6, back = "lines")
     
     # Rotation and zoom it
     rgl.viewpoint(60, 0, zoom = 0.5)
 }
-rgl.postscript("./test.pdf", fmt = "pdf", drawText = F)
-rgl.snapshot("./test.png", fmt = "png")
+rgl.postscript("./figure/sim_mean.pdf", fmt = "pdf")
+rgl.snapshot("./figure/sim_mean.png", fmt = "png")
 rgl.close()
 
 
+### Example curves of simulation for 3 types of data
+r3dDefaults$windowRect <- c(0, 0, 800, 350)
+open3d()
+mfrow3d(1, 3)   # par(mfrow = c(2, 1))
+for (sim.type in 1:3) {
+    # sim.type <- 3   # type of generated data
+    set.seed(5)
+    # 
+    ### Generate curves for each cluster
+    Lt <- list()
+    Ly <- list()
+    mu_list <- list()   # meanfunction for each cluster
+    xi_list <- list()   # true FPC scores
+    phi_list <- list()   # true eigenfunctions
+    cluster <- rep(1:k, n_k)   # cluster index
+    for (i in 1:k) {   # generate for each cluster
+        lambda <- 0.07^(seq_len(K) / 2)
+        basisType <- 'legendre01'
+        xiFun <- rnorm
+        sigma2 <- 0.01
+        muList <- list(
+            function(x) x * 2,
+            function(x) sin(x * 1 * pi) * pi / 2 * 0.6,
+            function(x) rep(0, length(x))
+        )
+        
+        if (i == 2) {
+            # basisType <- "fourier"
+            if (sim.type == 1) {
+                lambda <- (i*0.07)^(seq_len(K) / 2)
+                muList[[2]] <- function(x) (-sin(x * 1 * pi)) * pi / 2 * 0.6
+            } else if (sim.type == 2) {
+                lambda <- (i*0.07)^(seq_len(K) / 2)
+                muList[[2]] <- function(x) (cos(x * 4 * pi)) * pi / 2 * 0.6
+            } else if (sim.type == 3) {
+                lambda <- (i*0.07)^(seq_len(K) / 2)
+                muList[[2]] <- function(x) (sin(x * 3 * pi)) * pi / 2 * 0.6
+                # basisType <- "fourier"
+                # xiFun <- rcauchy
+                # muList[[2]] <- function(x) (-sin(x * 1 * pi)) * pi / 2 * 0.6
+                # muList[[2]] <- function(x) (cos(x * 5 * pi)) * pi / 2 * 0.6
+                # lambda <- ((i+1)*0.07)^(seq_len(K) / 2)
+            }
+        }
+        
+        pts <- seq(0, 1, length.out = m)
+        mfd <- structure(1, class = 'Sphere')
+        mu <- Makemu(mfd, muList, c(0, 0, 1), pts)
+        
+        # Generate samples
+        samp <- MakeMfdProcess(mfd = mfd, 
+                               n = n_k[i], 
+                               mu = mu, 
+                               pts = pts, 
+                               K = K, 
+                               xiFun = xiFun,
+                               lambda = lambda, 
+                               basisType = basisType, 
+                               sigma2 = sigma2)
+        spSamp <- array2list(samp$X, samp$T)
+        Ly <- c(Ly, spSamp$Ly)
+        Lt <- c(Lt, spSamp$Lt)
+        mu_list <- c(mu_list, list(mu))
+        xi_list <- c(xi_list, list(samp$xi))
+        phi_list <- c(phi_list, list(samp$phi))
+    }
+    
+    
+    ### 3D plot
+    # Cluster 1
+    plot3d(t(mu_list[[1]]), col = 1, type = "l", lwd = 3,
+           xlab = '', ylab = '', zlab = '', axes = FALSE,
+           xlim = c(-1, 1), ylim = c(-1, 1), zlim = c(-1, 1))
+    # plot3d(t(mu_list[[1]]), col = 1, size = 3, 
+    #        xlab = '', ylab = '', zlab = '', axes = FALSE,
+    #        xlim = c(-1, 1), ylim = c(-1, 1), zlim = c(-1, 1))
+    # plot3d(t(mu_list[[1]]), type = "l", col = 1, lwd = 2, add = T)
+    
+    # Cluster 2
+    # plot3d(t(mu_list[[2]]), col = 2, size = 3, add = T)
+    plot3d(t(mu_list[[2]]), type = "l", col = 2, lwd = 3, add = T)
+    # rgl.spheres(0, 0, 0, radius = 0.99, col = 'gray', alpha = 0.6, back = 'lines')
+    
+    # First 10 trajectories
+    for (i in c(sample(1:50, 10),
+                sample(51:100, 10))) {
+        x1 <- t(Ly[[i]])
+        plot3d(x1, type = "l", col = cluster[i], lwd = 1, add = T)
+    }
+    rgl.spheres(0, 0, 0, radius = 0.99, col = "gray", alpha = 0.3, back = "lines",
+                fastTransparency = F, add = T)
+    # rgl.spheres(0, 0, 0, radius = 1, col = "gray", alpha = 0.6, back = "lines")
+    
+    # Rotation and zoom it
+    rgl.viewpoint(60, 0, zoom = 0.5)
+}
+rgl.postscript("./figure/sim_sample.pdf", fmt = "pdf")
+rgl.snapshot("./figure/sim_sample.png", fmt = "png")
+rgl.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
 ### Plot trajectories on sphere
-library(rgl)
 # Cluster 1
 plot3d(t(mu_list[[1]]), col = 1, size = 5, 
        # xlab = 'x', ylab = 'y', zlab = 'z',
@@ -143,7 +256,8 @@ plot3d(t(mu_list[[1]]), type = "l", col = 1, lwd = 3, add = T)
 # Cluster 2
 plot3d(t(mu_list[[2]]), col = 2, size = 5, add = T)
 plot3d(t(mu_list[[2]]), type = "l", col = 2, lwd = 3, add = T)
-rgl.spheres(0, 0, 0, radius = 0.99, col = 'gray', alpha = 0.6, back = 'lines')
+rgl.spheres(0, 0, 0, radius = 0.99, col = 'gray', alpha = 0.6, back = 'lines',
+            fastTransparency = F)
 
 
 
@@ -153,7 +267,8 @@ for (i in c(1:10,
     x1 <- t(Ly[[i]])
     plot3d(x1, type = "l", col = cluster[i], lwd = 1, add = T)
 }
-rgl.spheres(0, 0, 0, radius = 1, col = 'gray', alpha = 0.6, back = 'lines')
+rgl.spheres(0, 0, 0, radius = 1, col = 'gray', alpha = 0.6, back = 'lines',
+            fastTransparency = F)
 
 
 title3d("Case 1", col = "black", line = 3)
@@ -172,10 +287,11 @@ for (i in 1:3) {
            xlab = '', ylab = '', zlab = '', axes = FALSE,
            xlim = c(-1, 1), ylim = c(-1, 1), zlim = c(-1, 1))
     plot3d(t(mu_list[[1]]), type = "l", col = 1, lwd = 3, add = T)
-    rgl.spheres(0, 0, 0, radius = 0.99, col = 'gray', alpha = 0.6, back = 'lines')
+    rgl.spheres(0, 0, 0, radius = 0.99, col = 'gray', alpha = 0.6, 
+                back = 'lines', fastTransparency = F)
     rgl.viewpoint(60, 0, zoom = 0.5)
-    title3d("Case 1", col = "black", line = 3)
-    mtext3d("Case 1")
+    # title3d("Case 1", col = "black", line = 3)
+    # mtext3d("Case 1")
 }
 rgl.close()
 
