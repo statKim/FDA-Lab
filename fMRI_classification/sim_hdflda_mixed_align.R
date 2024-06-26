ddacd952124bdfd541f88201916f0da6108f1e96
statKim <hyunsung1021@gmail.com>
2024-06-26 19:17:17 +0900

    [Modify] hdflda glmnet으로 수정

--- fMRI_classification/sim_hdflda_mixed_align.R
+++ fMRI_classification/sim_hdflda_mixed_align.R
@@ -24,16 +24,20 @@ for (i in 1:length(file_list)) {
 }
 dim(X)
 
+# Scalar variabls of 191 subjects
+X2 <- read.csv("./fMRI_Classification/X2.csv", header = T)
+colnames(X2) <- c("Gender","Age")
+X2$Gender <- ifelse(X2$Gender == "Male", 1, 0)
+
+# Remove 2 outlying curves
+X <- X[-c(33, 123), , ]
+y <- y[-c(33, 123)]
+X2 <- X2[-c(33, 123), ]
+
 n <- dim(X)[1]   # number of curves
 m <- dim(X)[2]   # number of timepoints
 p <- dim(X)[3]  # number of functional variables
 gr <- seq(0, 1, length.out = m)
-
-
-# Class label of 191 subjects
-X2 <- read.csv("./fMRI_Classification/X2.csv", header = T)
-colnames(X2) <- c("Gender","Age")
-X2$Gender <- ifelse(X2$Gender == "Male", 1, 0)
 
 # Curve length
 X_length <- matrix(0, n, p)
@@ -46,27 +50,38 @@ colnames(X_length) <- paste0("length_", 1:p)
 
 # Fast Fourier Transform with smoothing splines
 # Guo, X., Li, Y., & Hsing, T. (2023). An RKHS Approach for Variable Selection in High-dimensional Functional Linear Models. arXiv preprint arXiv:2310.14419.
-X_sm <- X
+X_fft <- X
+X_fft_sm <- X
 for (i in 1:p) {
   print(i)
-  X_fft <- apply(X[, , i], 1, function(x) {
+  X_i_fft <- apply(X[, , i], 1, function(x) {
     Mod(fft(x)) * (2/m)
     # smooth.spline(Mod(fft(x)) * (2/m))$y
   })
   
-  X_fft_sm <- apply(X[, , i], 1, function(x) {
+  X_i_fft_sm <- apply(X[, , i], 1, function(x) {
     # Mod(fft(x)) * (2/m)
     smooth.spline(Mod(fft(x)) * (2/m))$y
   })
   
-  X[, , i] <- t(X_fft)
-  X_sm[, , i] <- t(X_fft_sm)
+  X_fft[, , i] <- t(X_i_fft)
+  X_fft_sm[, , i] <- t(X_i_fft_sm)
 }
 
-i <- 82
-par(mfrow = c(2, 1))
-matplot(t(X[, , i]), type = "l")
-matplot(t(X_sm[, , i]), type = "l")
+# i <- 82
+# par(mfrow = c(3, 1))
+# matplot(gr, t(X[(y == 0)[1:15], , i]), type = "l", col = 1, lty = 1, lwd = 0.5,
+#         xlab = "Time", ylab = "", main = "Raw Furves")
+# matlines(gr, t(X[(y == 1)[1:15], , i]), col = 2, lty = 1, lwd = 0.5)
+# grid()
+# matplot(t(X_fft[(y == 0)[1:15], , i]), type = "l", col = 1, lty = 1, lwd = 0.5,
+#         xlab = "Hz", ylab = "", main = "Periodogram using FFT")
+# matlines(t(X_fft[(y == 0)[1:15], , i]), col = 2, lty = 1, lwd = 0.5)
+# grid()
+# matplot(t(X_fft_sm[(y == 0)[1:15], , i]), type = "l", col = 1, lty = 1, lwd = 0.5,
+#         xlab = "Hz", ylab = "", main = "Smoothed Periodogram using FFT")
+# matlines(t(X_fft_sm[(y == 0)[1:15], , i]), col = 2, lty = 1, lwd = 0.5)
+# grid()
 
 
 # Add curve length into scalar covariates
@@ -79,17 +94,18 @@ X2[, -1] <- scale(X2[, -1])
 ##################################################
 ### Classification
 ##################################################
+library(hdfda)
+library(doParallel)
 basis <- "bspline"
 n_basis_list <- c(4:9, seq(10, min(40, round(m/2)), by = 1))
 
 # Parallel computing
 # cl <- makePSOCKcluster(detectCores()/2)
-library(doParallel)
 cl <- makePSOCKcluster(40)
 registerDoParallel(cl)
 
-compare_methods <- c("hdflda_fft","hdflda_fft_sm","hdflda_fft_mix","hdflda_fft_sm_mix")
-
+compare_methods <- c("hdflda","hdflda_fft","hdflda_fft_sm",
+                     "hdflda_mix","hdflda_fft_mix","hdflda_fft_sm_mix")
 num_sim <- 100
 model_obj <- list()
 pred_list <- list()
@@ -109,8 +125,11 @@ for (sim in 1:num_sim) {
   y_train <- y[idx_train]
   y_test <- y[-idx_train]
   
-  X_sm_train <- X_sm[idx_train, , ]
-  X_sm_test <- X_sm[-idx_train, , ]
+  X_fft_train <- X_fft[idx_train, , ]
+  X_fft_test <- X_fft[-idx_train, , ]
+  
+  X_fft_sm_train <- X_fft_sm[idx_train, , ]
+  X_fft_sm_test <- X_fft_sm[-idx_train, , ]
   
   X2_train <- X2[idx_train, ]
   X2_test <- X2[-idx_train, ]
@@ -123,20 +142,31 @@ for (sim in 1:num_sim) {
   pred_mat <- data.frame(y_test = y_test)
   model_fit <- list()
   
-  # # High-dimensional functional LDA
-  # cv.fit <- cv.hdflda(X_train, y_train, X2_train)
-  # pred <- predict(cv.fit$opt_fit, X_test, X2_test)
-  # pred_mat$hdflda <- pred
+  # hdflda
+  set.seed(sim)
+  start_time <- Sys.time()
+  tryCatch({
+    cv.fit <- cv.hdflda(X_train, y_train, n_basis_list = n_basis_list)
+    pred <- predict(cv.fit$opt_fit, X_test)
+  },
+  error = function(err){
+    print(err)
+    error_TF[sim] <<- TRUE
+    cv.fit <<- NULL
+    pred <<- rep(0, length(y_test))
+  })
+  end_time <- Sys.time()
+  print(end_time - start_time)
+  pred_mat$hdflda <- pred
+  model_fit$hdflda <- cv.fit
+  
   
   # hdflda + FFT
   set.seed(sim)
   start_time <- Sys.time()
   tryCatch({
-    # cv.fit <- cv.hdflda(X_train, y_train, 
-    #                     n_basis_list = n_basis_list, measure = "cross.entropy")
-    cv.fit <- cv.hdflda(X_train, y_train, n_basis_list = n_basis_list)
-    # cv.fit <- cv.hdflda(X_train, y_train)
-    pred <- predict(cv.fit$opt_fit, X_test)
+    cv.fit <- cv.hdflda(X_fft_train, y_train, n_basis_list = n_basis_list)
+    pred <- predict(cv.fit$opt_fit, X_fft_test)
   },
   error = function(err){
     print(err)
@@ -149,14 +179,13 @@ for (sim in 1:num_sim) {
   pred_mat$hdflda_fft <- pred
   model_fit$hdflda_fft <- cv.fit
   
+  
   # hdflda + FFT + smoothing
   set.seed(sim)
   start_time <- Sys.time()
   tryCatch({
-    # cv.fit <- cv.hdflda(X_sm_train, y_train, 
-    #                     n_basis_list = n_basis_list, measure = "cross.entropy")
-    cv.fit <- cv.hdflda(X_sm_train, y_train)
-    pred <- predict(cv.fit$opt_fit, X_sm_test)
+    cv.fit <- cv.hdflda(X_fft_sm_train, y_train, n_basis_list = n_basis_list)
+    pred <- predict(cv.fit$opt_fit, X_fft_sm_test)
   },
   error = function(err){
     print(err)
@@ -169,15 +198,32 @@ for (sim in 1:num_sim) {
   pred_mat$hdflda_fft_sm <- pred
   model_fit$hdflda_fft_sm <- cv.fit
   
+  
+  # hdflda + scalar covariates (include curve length)
+  set.seed(sim)
+  start_time <- Sys.time()
+  tryCatch({
+    cv.fit <- cv.hdflda(X_train, y_train, X2_train, n_basis_list = n_basis_list)
+    pred <- predict(cv.fit$opt_fit, X_test, X2_test)
+  },
+  error = function(err){
+    print(err)
+    error_TF[sim] <<- TRUE
+    cv.fit <<- NULL
+    pred <<- rep(0, length(y_test))
+  })
+  end_time <- Sys.time()
+  print(end_time - start_time)
+  pred_mat$hdflda_mix <- pred
+  model_fit$hdflda_mix <- cv.fit
+  
+  
   # hdflda + FFT + scalar covariates (include curve length)
   set.seed(sim)
   start_time <- Sys.time()
   tryCatch({
-    # cv.fit <- cv.hdflda(X_train, y_train, X2_train,
-    #                     n_basis_list = n_basis_list, measure = "cross.entropy")
-    cv.fit <- cv.hdflda(X_train, y_train, X2_train, n_basis_list = n_basis_list)
-    # cv.fit <- cv.hdflda(X_train, y_train, X2_train)
-    pred <- predict(cv.fit$opt_fit, X_test, X2_test)
+    cv.fit <- cv.hdflda(X_fft_train, y_train, X2_train, n_basis_list = n_basis_list)
+    pred <- predict(cv.fit$opt_fit, X_fft_test, X2_test)
   },
   error = function(err){
     print(err)
@@ -190,15 +236,13 @@ for (sim in 1:num_sim) {
   pred_mat$hdflda_fft_mix <- pred
   model_fit$hdflda_fft_mix <- cv.fit
   
+  
   # hdflda + FFT + smoothing + scalar covariates (include curve length)
   set.seed(sim)
   start_time <- Sys.time()
   tryCatch({
-    # cv.fit <- cv.hdflda(X_sm_train, y_train, X2_train, 
-    #                     n_basis_list = n_basis_list, measure = "cross.entropy")
-    cv.fit <- cv.hdflda(X_sm_train, y_train, X2_train, n_basis_list = n_basis_list)
-    # cv.fit <- cv.hdflda(X_sm_train, y_train, X2_train)
-    pred <- predict(cv.fit$opt_fit, X_sm_test, X2_test)
+    cv.fit <- cv.hdflda(X_fft_sm_train, y_train, X2_train, n_basis_list = n_basis_list)
+    pred <- predict(cv.fit$opt_fit, X_fft_sm_test, X2_test)
   },
   error = function(err){
     print(err)
@@ -220,9 +264,10 @@ for (sim in 1:num_sim) {
   print(round(acc_sim[sim, ], 3))
   print(round(colMeans(acc_sim[1:sim, ]), 3))
   
-  if (sim %% 5 == 0) {
-    save(model_obj, pred_list, acc_sim, file = "RData/sim_hdflda_align.RData")
-  }
+  # if (sim %% 2 == 0) {
+  #   save(model_obj, pred_list, acc_sim, error_TF, file = "RData/sim_hdflda_align.RData")
+  # }
+  save(model_obj, pred_list, acc_sim, error_TF, file = "RData/sim_hdflda_align.RData")
 }
 stopCluster(cl)
 unregister()
